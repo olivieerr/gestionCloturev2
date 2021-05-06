5725419dda797efe5dea73e76c5a9e2c67eb20f5
Olivieerr <olivieerr@site-en-vrac.com>
2021-05-06 19:48:06 +0200

    Mise en place du paquet NuGet MySql Mise en place du Timer : fonction "rebours" qui declanche les traitements sur la BDD setTimer qui met en place le Tiemer Reste à faire : changer le nom de l'objet "toto" et mettre en place un constante pour la durée du Timer redefinir le démarage du projet et commenter toutes les "Console.Write()"

--- gestionCloturev2/Program.cs
+++ gestionCloturev2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 
 //using MySql.Data.MySqlClient;
 using Gsb.gestionCloturev2.logic;
@@ -12,15 +13,36 @@ namespace gestionCloturev2
         //date de test
         //static DateTime dateTest = new DateTime(2021, 01, 31);     
 
+        private static System.Timers.Timer timer;
 
-
-        static void Main(string[] args)
+        private static void rebours(Object source, ElapsedEventArgs e)
         {
-            Console.WriteLine("Hello World!"); //a supprimer
             ClotureLogic toto = new ClotureLogic();
 
             toto.clotureFicheFrais();
             toto.miseEnRemboursement();
+        }
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            timer = new System.Timers.Timer(10000);
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += rebours;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!"); //a supprimer
+
+            rebours(null,null);
+            SetTimer();
+
+            Console.ReadLine();
+            timer.Stop();
+            timer.Dispose();
 
         }
     }
